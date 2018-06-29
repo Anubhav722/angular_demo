84ffe91f9e0cd62607674116e2b4ff7940b69e7c
Anubhav722 <anubhavs286@gmail.com>
2018-06-29 16:53:59 +0530

    add ngFor directive

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -32,16 +32,37 @@ import { Component, OnInit } from '@angular/core';
   		<div *ngSwitchCase="'green'">You picked green color</div>
   		<div *ngSwitchDefault>Pick Again</div>
   	</div>
+
+
+  	<div *ngFor="let color of colors; index as i">
+  		<h2>{{i}} {{color}}</h2>
+  	</div>
+  	<div *ngFor="let color of colors; first as f">
+  		<h2>{{f}} {{color}}</h2>
+  	</div>
+  	<div *ngFor="let color of colors; last as l">
+  		<h2>{{l}} {{color}}</h2>
+  	</div>
+  	<div *ngFor="let color of colors; odd as o">
+  		<h2>{{o}} {{color}}</h2>
+  	</div>
+  	<div *ngFor="let color of colors; even as e">
+  		<h2>{{e}} {{color}}</h2>
+  	</div>
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   // if case
-  displayName = false;
+  displayName = true;
 
   // Switch case
   public color = "";
+
+  // ngFor directive
+  public colors = ["red", "blue", "green", "yellow"];
+
   constructor() { }
 
   ngOnInit() {
